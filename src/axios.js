28bc3e003b26cf0edf6d8ee462f6544f15c32df1
Abdellah-SLIMANI <morscor21@gmail.com>
2021-11-15 00:00:42 +0100

    axios probl

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,6 @@
 import axios from 'axios'
 
-// const axiosInstance = axios.create({
-//     baseURL
-// })
+const axiosInstance = axios.create()
 
 axiosInstance.interceptors.response.use(
     (response) => response,
