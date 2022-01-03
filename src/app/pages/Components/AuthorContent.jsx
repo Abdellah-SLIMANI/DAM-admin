629aa01d23a106e8da9a41cb33049165a6c59bad
Abdellah-SLIMANI <morscor21@gmail.com>
2022-01-03 17:25:48 +0100

    biblio a la ligne

--- src/app/pages/Components/AuthorContent.jsx
+++ src/app/pages/Components/AuthorContent.jsx
@@ -13,6 +13,9 @@ export default function AuthorContent({author}) {
                                         if(mapAuthorProps[key].name === 'liens' && author[key] != '[]'){
                                           return <div style={{fontSize: '16px'}}><span style={{fontStyle: 'italic'}}>{mapAuthorProps[key].label + " : "}</span><span >{author[key] && author[key].map(lien => lien.titre + ', ')}</span><br /></div>
                                         }
+                                        if(mapAuthorProps[key].name === 'bibliographie'){
+                                            return <div style={{fontSize: '16px'}}><span style={{fontStyle: 'italic'}}>{mapAuthorProps[key].label + " : "}<span >{author[key] && author[key].map(biblio => <><br></br> {biblio}</>)}</span></span></div>
+                                        }
                                         return <div style={{fontSize: '16px'}}><span style={{fontStyle: 'italic'}}>{mapAuthorProps[key].label + " : "}</span><span >{author[key]}</span><br /></div>             
                                 }
                                 )
